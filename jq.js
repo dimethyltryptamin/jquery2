b5f94f95555214c1048317741d3a1e4986cf4654
dimethyltryptamin <brandbergway@protonmail.com>
2018-01-16 19:26:33 +0100

    Gotowe

--- jq.js
+++ jq.js
@@ -1,11 +1,15 @@
-$(function(){
-	$("span:even").css('color', 'red');
-	$('p').each(function(index, element) {
-		var button = '<button class="btn" data-tmp="' + index + '">Click me</button>';
-		$(element).append(button);
-	});
-	
-	$("button").click(function(){
-		alert($(this).attr("data-tmp"));
-	});
+$(function () {
+	var carouselList = $("ul");
+
+	function changeSlide() {
+		carouselList.animate({'marginLeft': '-200px'}, 500, moveFirstSlide);
+	}
+
+	function moveFirstSlide() {
+		var firstItem = carouselList.find("li:first");
+		var lastItem = carouselList.find("li:last");
+		lastItem.after(firstItem);
+		carouselList.css({marginLeft: 0});
+	}
+	setInterval(changeSlide, 3000);
 });
