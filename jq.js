aa18a223173b65ea4213ed66e503aa99f14a963a
dimethyltryptamin <brandbergway@protonmail.com>
2018-01-16 13:12:18 +0100

    Start

---
+++ jq.js
@@ -0,0 +1,11 @@
+$(function(){
+	$("span:even").css('color', 'red');
+	$('p').each(function(index, element) {
+		var button = '<button class="btn" data-tmp="' + index + '">Click me</button>';
+		$(element).append(button);
+	});
+	
+	$("button").click(function(){
+		alert($(this).attr("data-tmp"));
+	});
+});
